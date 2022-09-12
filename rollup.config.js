6e193c9653a100894baf0498e65c86412d85ffae
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2022-09-12 12:20:15 +0800

    build: migrate to rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import rollupTypescript from "@rollup/plugin-typescript";
+import { defineConfig } from "rollup";
+import { terser } from "rollup-plugin-terser";
+
+export default defineConfig({
+  input: "src/index.ts",
+  plugins: [rollupTypescript()],
+  output: [
+    {
+      file: "./dist/index.mjs",
+      format: "esm",
+    },
+    {
+      file: "./dist/index.min.mjs",
+      format: "esm",
+      sourcemap: "hidden",
+      plugins: [terser()],
+    },
+    {
+      file: "./dist/index.js",
+      format: "cjs",
+    },
+    {
+      file: "./dist/index.umd.js",
+      format: "umd",
+      name: "ts-nameof-proxy",
+    },
+  ],
+});
