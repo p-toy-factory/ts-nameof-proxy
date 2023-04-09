b04080e5d44d6248feee64f201a88f34ef626d43
Pink Champagne <45930107+PinkChampagne17@users.noreply.github.com>
2023-04-09 17:30:56 +0800

    build: only generate index.d.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,37 @@
+import dts from "rollup-plugin-dts";
 import rollupTypescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 import { terser } from "rollup-plugin-terser";
 
-export default defineConfig({
-  input: "src/index.ts",
-  plugins: [rollupTypescript()],
-  output: [
-    {
-      file: "./dist/index.mjs",
-      format: "esm",
-    },
-    {
-      file: "./dist/index.min.mjs",
-      format: "esm",
-      sourcemap: "hidden",
-      plugins: [terser()],
-    },
-    {
-      file: "./dist/index.js",
-      format: "cjs",
-    },
-    {
-      file: "./dist/index.umd.js",
-      format: "umd",
-      name: "ts-nameof-proxy",
-    },
-  ],
-});
+export default [
+  defineConfig({
+    input: "src/index.ts",
+    plugins: [rollupTypescript()],
+    output: [
+      {
+        file: "./dist/index.mjs",
+        format: "esm",
+      },
+      {
+        file: "./dist/index.min.mjs",
+        format: "esm",
+        sourcemap: "hidden",
+        plugins: [terser()],
+      },
+      {
+        file: "./dist/index.js",
+        format: "cjs",
+      },
+      {
+        file: "./dist/index.umd.js",
+        format: "umd",
+        name: "ts-nameof-proxy",
+      },
+    ],
+  }),
+  defineConfig({
+    input: "src/index.ts",
+    output: [{ file: "dist/index.d.ts", format: "es" }],
+    plugins: [dts()],
+  }),
+];
